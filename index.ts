b354eb3e88f5b151e922a1468fae6d5ed59251d1
Alexey Shevtsov <a.shevtsov@itransition.com>
2022-12-28 20:34:49 +0100

    init: install and config lint

---
+++ index.ts
@@ -0,0 +1,6 @@
+const revenue = 1000;
+const bonus = 500;
+
+const total: number = revenue + bonus;
+
+console.info(total);
---
+++ src/example.ts
@@ -0,0 +1,11 @@
+class User {
+    name: string;
+    age!: number;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const user = new User('John');
+console.log(user)
